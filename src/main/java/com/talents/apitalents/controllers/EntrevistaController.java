ac6327884b67bfc080f2f47fb693d3669b461543
Pedro Santos <pedrobsantos98@outlook.com>
2020-10-05 21:53:46 -0300

    Secutiry Config

--- src/main/java/com/talents/apitalents/controllers/EntrevistaController.java
+++ src/main/java/com/talents/apitalents/controllers/EntrevistaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/entrevistas")
+@RequestMapping(value = "/entrevistas")
 public class EntrevistaController {
 
     @Autowired
--- src/main/java/com/talents/apitalents/controllers/EntrevistadorController.java
+++ src/main/java/com/talents/apitalents/controllers/EntrevistadorController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/entrevistadores")
+@RequestMapping(value = "/entrevistadores")
 public class EntrevistadorController {
 
     @Autowired
